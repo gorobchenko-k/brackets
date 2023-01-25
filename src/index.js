48a488630ac03a09af0a8b0074734e63c0f75e3e
gorobchenko-k <gorobchenko.es@mail.ru>
2023-01-25 14:05:33 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let sameBracket = [];
+  const brackets = bracketsConfig.reduce((obj, item) => {
+    obj[item[1]] = item[0];
+    if (item[0] === item[1]) sameBracket.push(item[0]);
+    return obj;
+  }, {});
+
+  let result = [];
+  // let firstBracket = true;
+
+  for (let index = 0; index < str.length; index++) {
+    let char = str[index];
+    // if (sameBracket.includes(char)) {
+    // char = firstBracket ? '[' : ']';
+    // firstBracket = !firstBracket;
+    // brackets[']'] = '[';
+    // }
+    if (char in brackets && result.length > 0 && brackets[char] === result[result.length - 1]) {
+      if (brackets[char] !== result.pop()) {
+        return false;
+      }
+    } else {
+      result.push(char);
+    }
+  }
+
+  return result.length === 0;
 }
